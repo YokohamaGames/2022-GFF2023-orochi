c7621011dd081af6058aff69051de830a8ffdf39
murakamikoichi <koukoro955@gmail.com>
2022-09-20 21:42:17 +0900

    [update] Player1.cs
    プレイヤーの移動を調整中

--- Assets/Scripts/Player1.cs
+++ Assets/Scripts/Player1.cs
@@ -6,9 +6,16 @@ public class Player1 : MonoBehaviour
 {
     [SerializeField] private Vector3 velocity;              // 移動方向
     [SerializeField] private float moveSpeed = 5.0f;        // 移動速度
+
+    
+
     [SerializeField]
     [Tooltip("動くスピードを指定")]
-    private float speed = 1;
+    private float speed = 10.0f;
+
+    [SerializeField]
+    [Tooltip("ジャンプ力を指定")]
+    private float upForce = 200f;
 
     [SerializeField]
     private bool isGrounded = false;
@@ -49,6 +56,7 @@ public class Player1 : MonoBehaviour
 
     void Start()
     {
+       // isGrounded = true;
         rigidbody = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
 
@@ -135,40 +143,21 @@ public class Player1 : MonoBehaviour
 
         }
 
-        // WASD入力から、XZ平面(水平な地面)を移動する方向(velocity)を得ます
-        velocity = Vector3.zero;
-        if (Input.GetKey(KeyCode.W))
-            velocity.z += speed;
-        if (Input.GetKey(KeyCode.A))
-            velocity.x -= speed;
-        if (Input.GetKey(KeyCode.S))
-            velocity.z -= speed;
-        if (Input.GetKey(KeyCode.D))
-            velocity.x += speed;
-
-        // 速度ベクトルの長さを1秒でmoveSpeedだけ進むように調整します
         velocity = velocity.normalized * moveSpeed * Time.deltaTime;
-
-        // いずれかの方向に移動している場合
-        if (velocity.magnitude > 0)
+        if (Input.GetButton("Left"))
         {
-            // プレイヤーの位置(transform.position)の更新
-            // 移動方向ベクトル(velocity)を足し込みます
-            transform.position += velocity;
+            velocity.x -= 1;
         }
+        transform.position += velocity;
 
-
-
-
-
-        Vector3 diff = transform.position - latestPos;   //前回からどこに進んだかをベクトルで取得
-        latestPos = transform.position;  //前回のPositionの更新
-
-        //ベクトルの大きさが0.01以上の時に向きを変える処理をする
-        if (diff.magnitude > 0.01f)
-        {
-            transform.rotation = Quaternion.LookRotation(diff); //向きを変更する
-        }
+        // if(isGrounded == true)
+        // {
+        //     if(Input.GetKeyDown("space"))
+        //     {
+        //         isGrounded = false;
+        //         rigidbody.AddForce(new Vector3(0, upForce, 0));
+        //     }
+        // }
     }
 
     void UpdateForWalkState()
