9ba87243bceee3b36d4a7ab862a3d0fc713596d6
murakamikoichi <koukoro955@gmail.com>
2022-09-28 14:31:02 +0900

    [add] Add TitleScene and Scripts
    タイトルシーン（シーンだけ）と空のスクリプトの追加

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -1,54 +1,254 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class player : MonoBehaviour
+public class Player : MonoBehaviour
 {
     [SerializeField] private Vector3 velocity;              // 移動方向
-    [SerializeField] private float moveSpeed = 5.0f;        // 移動速度
 
-    private Vector3 latestPos;  //前回のPosition
+    [SerializeField]
+    private Vector3 com;
+
+    [SerializeField]
+    [Tooltip("動くスピードを指定")]
+    private float speed = 10.0f;
+
+    /* [SerializeField]
+     [Tooltip("ジャンプ力を指定")]
+     private float upForce = 200f;
+    */
+    [SerializeField]
+    private bool isGrounded = false;
+    /* [SerializeField]
+     private LayerMask groudLayer;
+    */
+
+
+    // プレイヤーの状態を表します
+    enum PlayerState
+    {
+        // 歩き
+        Walk,
+        // ジャンプの予備動作
+        JumpReady,
+        // ジャンプ中
+        Jumping,
+        // 回避中
+        Avoid,
+        // 攻撃
+        Attack,
+        // 大
+        Big,
+        // 中
+        Medium,
+        // 小
+        Small,
+        // ギミック系
+        G1, G2, G3,
+    }
+    // 現在のプレイヤーの状態
+    PlayerState currentState = PlayerState.Walk;
+
+    // コンポーネントを事前に参照しておく変数
+    new Rigidbody rigidbody;
+
+    [SerializeField]
+    private Animator animator = null;
+
+    // ユーザーからの入力
+    Vector2 moveInput = Vector2.zero;
+    Vector2 lookInput = Vector2.zero;
 
     void Start()
     {
-        
+        // isGrounded = true;
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.centerOfMass = com;
+    }
+
+    // Walkステートに遷移させます。
+    void SetWalkState()
+    {
+        currentState = PlayerState.Walk;
+    }
+
+    // JumpReadyステートに遷移させます。
+    void SetJumpReadyState()
+    {
+        currentState = PlayerState.JumpReady;
+    }
+
+    // Jumpingステートに遷移させます。
+    void SetJumpingState()
+    {
+        currentState = PlayerState.Jumping;
+    }
+
+    // Avoidステートに遷移させます。
+    void SetAvoidState()
+    {
+        currentState = PlayerState.Avoid;
+    }
+
+    // Attackステートに遷移させます。
+    void SetAttackState()
+    {
+        currentState = PlayerState.Attack;
+    }
+
+    // Bigステートに遷移させます。
+    void SetBigState()
+    {
+        currentState = PlayerState.Big;
+    }
+
+    // Mediumステートに遷移させます。
+    void SetMediumState()
+    {
+        currentState = PlayerState.Medium;
+    }
+
+    // Smallステートに遷移させます。
+    void SetSmallState()
+    {
+        currentState = PlayerState.Small;
     }
 
     void Update()
     {
-        // WASD入力から、XZ平面(水平な地面)を移動する方向(velocity)を得ます
-        velocity = Vector3.zero;
-        if (Input.GetKey(KeyCode.W))
-            velocity.z += 1;
-        if (Input.GetKey(KeyCode.A))
-            velocity.x -= 1;
-        if (Input.GetKey(KeyCode.S))
-            velocity.z -= 1;
-        if (Input.GetKey(KeyCode.D))
-            velocity.x += 1;
-
-        // 速度ベクトルの長さを1秒でmoveSpeedだけ進むように調整します
-        velocity = velocity.normalized * moveSpeed * Time.deltaTime;
-
-        // いずれかの方向に移動している場合
-        if (velocity.magnitude > 0)
+        switch (currentState)
         {
-            // プレイヤーの位置(transform.position)の更新
-            // 移動方向ベクトル(velocity)を足し込みます
-            transform.position += velocity;
+            case PlayerState.Walk:
+                UpdateForWalkState();
+                break;
+            case PlayerState.JumpReady:
+                UpdateForJumpReadyState();
+                break;
+            case PlayerState.Jumping:
+                UpdateForJumpingState();
+                break;
+            case PlayerState.Avoid:
+                UpdateForAvoidState();
+                break;
+            case PlayerState.Attack:
+                UpdateForAttackState();
+                break;
+            case PlayerState.Big:
+                UpdateForBigState();
+                break;
+            case PlayerState.Medium:
+                UpdateForMediumState();
+                break;
+            case PlayerState.Small:
+                UpdateForSmallState();
+                break;
         }
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    void UpdateForWalkState()
+    {
+        // プレイヤーの前後左右の移動
+        var velocity = Vector3.zero;
+        velocity = transform.forward * moveInput.y * speed;
+        velocity += transform.right * moveInput.x * speed;
+        velocity.y = rigidbody.velocity.y;
+        rigidbody.velocity = velocity;
+
+        // プレイヤーの方角を回転
+        transform.Rotate(0, lookInput.x, 0);
 
 
 
+        // if(isGrounded == true)
+        // {
+        //     if(Input.GetKeyDown("space"))
+        //     {
+        //         isGrounded = false;
+        //         rigidbody.AddForce(new Vector3(0, upForce, 0));
+        //     }
+        // }
+    }
 
+    void UpdateForJumpReadyState()
+    {
 
-        Vector3 diff = transform.position - latestPos;   //前回からどこに進んだかをベクトルで取得
-        latestPos = transform.position;  //前回のPositionの更新
+    }
 
-        //ベクトルの大きさが0.01以上の時に向きを変える処理をする
-        if (diff.magnitude > 0.01f)
+    void UpdateForJumpingState()
+    {
+
+    }
+
+    void UpdateForAvoidState()
+    {
+
+    }
+
+    void UpdateForAttackState()
+    {
+
+    }
+
+    void UpdateForBigState()
+    {
+
+    }
+
+    void UpdateForMediumState()
+    {
+
+    }
+
+    void UpdateForSmallState()
+    {
+
+    }
+
+    // ユーザーからのMoveアクションに対して呼び出されます。
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+
+        // アクションが始まった
+        /*if (context.started)
         {
-            transform.rotation = Quaternion.LookRotation(diff); //向きを変更する
+            Debug.Log($"started : {moveInput}");
         }
+        // アクションが継続中
+        else if (context.performed)
+        {
+            Debug.Log($"performed : {moveInput}");
+        }
+        // アクションが終了
+        else if (context.canceled)
+        {
+            Debug.Log($"canceled : {moveInput}");
+        }*/
+    }
+
+    // ユーザーからのMoveアクションに対して呼び出されます。
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
+
+        // アクションが始まった
+        /*if (context.started)
+        {
+            Debug.Log($"started : {lookInput}");
+        }
+        // アクションが継続中
+        else if (context.performed)
+        {
+            Debug.Log($"performed : {lookInput}");
+        }
+        // アクションが終了
+        else if (context.canceled)
+        {
+            Debug.Log($"canceled : {lookInput}");
+        }*/
     }
 }
