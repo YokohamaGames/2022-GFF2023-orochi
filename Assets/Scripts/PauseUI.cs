ea999e6da692c57d5f79839115cd1a8a07facfa2
murakamikoichi <koukoro955@gmail.com>
2022-09-28 15:22:37 +0900

    [add] Add PauseUI
    ポーズ画面のイメージを追加

--- Assets/Scripts/PauseUI.cs
+++ Assets/Scripts/PauseUI.cs
@@ -1,18 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	// ポーズを参照
+	[SerializeField]
+	private GameObject Panel = null;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// 初期選択状態に設定するボタンを指定します。
+	[SerializeField]
+	private Selectable PauseButton = null;
+	void Start()
+	{
+		Panel.SetActive(false);
+		PauseButton.Select();
+	}
+
+	// ゲームを止める
+	public void StopGame()
+	{
+		// ポーズの表示
+		Panel.SetActive(true);
+		// 停止
+		Time.timeScale = 0f;
+	}
+	// ゲームを再開させる
+	public void ReStartGame()
+	{
+		// ポーズの非表示
+		Panel.SetActive(false);
+		// 再開
+		Time.timeScale = 1f;
+	}
+
+	// Backが押されたらReStartGameを呼び出す
+	public void Back()
+	{
+		ReStartGame();
+	}
+
+	// Homeが押されたらsceneをTitleへ移行する
+	public void Home()
+	{
+		SceneManager.LoadScene("Title");
+	}
 }
