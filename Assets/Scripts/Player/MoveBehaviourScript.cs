a5c8f8d07a6f004bd598e2d67b3a5f6382f3f3b8
murakamikoichi <koukoro955@gmail.com>
2022-12-06 15:23:34 +0900

    [fix] Fix Player Collision
    プレイヤーの当たり判定を修正中

--- Assets/Scripts/Player/MoveBehaviourScript.cs
+++ Assets/Scripts/Player/MoveBehaviourScript.cs
@@ -486,7 +486,7 @@ public class MoveBehaviourScript : MonoBehaviour
             if (collision.CompareTag("Enemy_Weapon"))
             {
                 SE.Instance.Damaged();
-                GameObject effectplay = Instantiate(damaged, this.transform.position, Quaternion.identity);
+                //GameObject effectplay = Instantiate(damaged, this.transform.position, Quaternion.identity);
                 StageScene.Instance.Damage();
                 SetInvincible();
             }
