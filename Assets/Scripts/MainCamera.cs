436066665b638bd7a7842671ed9ea912458b24d1
murakamikoichi <koukoro955@gmail.com>
2022-12-20 18:17:52 +0900

    [update] Update Scripts
    namespaceを追加

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -5,55 +5,58 @@ using System.Collections.Generic;
 using UnityEngine.InputSystem;
 using Cinemachine;
 
-// プレイヤー追従カメラ
-public class MainCamera : MonoBehaviour
+namespace OROCHI
 {
-    // 1秒間で180度
-    public Vector2 rotationSpeed = new Vector2(180, 180);
-
-    [SerializeField]
-    CinemachineVirtualCamera vCam = null;
-    [SerializeField]
-    Cinemachine3rdPersonFollow follow = null;
-
-    private void Start()
+    // プレイヤー追従カメラ
+    public class MainCamera : MonoBehaviour
     {
-        vCam = GetComponent<CinemachineVirtualCamera>();
-        if (vCam != null)
-        {
-            follow = vCam.GetCinemachineComponent<Cinemachine3rdPersonFollow>();
-        }
-    }
+        // 1秒間で180度
+        public Vector2 rotationSpeed = new Vector2(180, 180);
 
-    private void FixedUpdate()
-    {
-        if (vCam != null)
+        [SerializeField]
+        CinemachineVirtualCamera vCam = null;
+        [SerializeField]
+        Cinemachine3rdPersonFollow follow = null;
+
+        private void Start()
         {
-            Transform target = vCam.Follow;
-            if (target != null)
+            vCam = GetComponent<CinemachineVirtualCamera>();
+            if (vCam != null)
             {
-                // ターゲットの角度をオイラー角度(x, y, z)で取得
-                Vector3 targetEulerAngles = target.rotation.eulerAngles;
-
-                // y軸の回転を変える
-                targetEulerAngles.y += cameraRotationInput.x * rotationSpeed.y * Time.fixedDeltaTime;
-                // オイラー角度をクオータニオンに変換して追跡ターゲットの回転を変える
-                target.transform.rotation = Quaternion.Euler(targetEulerAngles);
-
+                follow = vCam.GetCinemachineComponent<Cinemachine3rdPersonFollow>();
             }
         }
 
-    }
+        private void FixedUpdate()
+        {
+            if (vCam != null)
+            {
+                Transform target = vCam.Follow;
+                if (target != null)
+                {
+                    // ターゲットの角度をオイラー角度(x, y, z)で取得
+                    Vector3 targetEulerAngles = target.rotation.eulerAngles;
 
-    Vector2 cameraRotationInput = Vector2.zero;
+                    // y軸の回転を変える
+                    targetEulerAngles.y += cameraRotationInput.x * rotationSpeed.y * Time.fixedDeltaTime;
+                    // オイラー角度をクオータニオンに変換して追跡ターゲットの回転を変える
+                    target.transform.rotation = Quaternion.Euler(targetEulerAngles);
 
-    void Look(Vector2 input)
-    {
-        cameraRotationInput = input;
-    }
+                }
+            }
 
-    public void Onlook(InputAction.CallbackContext context)
-    {
-        Look(context.ReadValue<Vector2>());
+        }
+
+        Vector2 cameraRotationInput = Vector2.zero;
+
+        void Look(Vector2 input)
+        {
+            cameraRotationInput = input;
+        }
+
+        public void Onlook(InputAction.CallbackContext context)
+        {
+            Look(context.ReadValue<Vector2>());
+        }
     }
 }
--- Assets/Scripts/TitleScene.cs
+++ Assets/Scripts/TitleScene.cs
@@ -3,68 +3,71 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using System.Collections;
 
-public class TitleScene : MonoBehaviour
+namespace OROCHI
 {
-    // Titleを参照
-    [SerializeField]
-    private GameObject Title = null;
-
-    // Titleボタンを参照
-    [SerializeField]
-    private Selectable TitleButton = null;
-
-    // 次のシーンを読み込み可能な場合はtrue、それ以外はfalse
-    bool isLoadable = false;
-
-    Animator animator;
-    static readonly int FadeOutId = Animator.StringToHash("FadeOut");
-
-    /* StageSelectを参照
-    [SerializeField]
-    private GameObject StageSelect = null;
-       selectボタンを参照
-    [SerializeField]
-    private Selectable SelectButton = null;
-    */
-
-    private void Start()
+    public class TitleScene : MonoBehaviour
     {
-        Title.SetActive(true);
-        // StageSelect.SetActive(false);
-        TitleButton.Select();
-        animator = GetComponent<Animator>();
+        // Titleを参照
+        [SerializeField]
+        private GameObject Title = null;
+
+        // Titleボタンを参照
+        [SerializeField]
+        private Selectable TitleButton = null;
+
+        // 次のシーンを読み込み可能な場合はtrue、それ以外はfalse
+        bool isLoadable = false;
+
+        Animator animator;
+        static readonly int FadeOutId = Animator.StringToHash("FadeOut");
+
+        /* StageSelectを参照
+        [SerializeField]
+        private GameObject StageSelect = null;
+           selectボタンを参照
+        [SerializeField]
+        private Selectable SelectButton = null;
+        */
+
+        private void Start()
+        {
+            Title.SetActive(true);
+            // StageSelect.SetActive(false);
+            TitleButton.Select();
+            animator = GetComponent<Animator>();
+        }
+
+        // Titleを非表示、StageSelectを表示
+        public void Stageselect()
+        {
+            // SelectButton.Select();
+            Title.SetActive(false);
+            // StageSelect.SetActive(true);
+        }
+
+        // ゲームを終了する
+        public void Exit()
+        {
+            StartCoroutine(OnStart());
+            Application.Quit();
+        }
+
+        // Stageを読み込む
+
+
+        public void LoadStage()
+        {
+            StartCoroutine(OnStart());
+            SceneManager.LoadScene("Stage");
+
+        }
+
+        IEnumerator OnStart()
+        {
+            animator.SetTrigger(FadeOutId);
+            yield return new WaitForSeconds(2);
+            isLoadable = true;
+        }
+
     }
-
-    // Titleを非表示、StageSelectを表示
-    public void Stageselect()
-    {
-        // SelectButton.Select();
-        Title.SetActive(false);
-        // StageSelect.SetActive(true);
-    }
-
-    // ゲームを終了する
-    public void Exit()
-    {
-        StartCoroutine(OnStart());
-        Application.Quit();
-    }
-
-    // Stageを読み込む
-
-
-    public void LoadStage()
-    {
-        StartCoroutine(OnStart());
-        SceneManager.LoadScene("Stage");
-
-    }
-
-    IEnumerator OnStart()
-    {
-        animator.SetTrigger(FadeOutId);
-        yield return new WaitForSeconds(2);
-        isLoadable = true;
-    }
-
 }
